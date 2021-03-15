0897c1a82b45397cfe6abd59eceaa9953da8cd4a
DESKTOP-P31B6DC\isael <rogelioisael@hotmail.com>
2021-03-15 02:10:40 -0400

    Add NumeralJS

--- scraper.js
+++ scraper.js
@@ -1,15 +1,16 @@
 const puppeteer = require('puppeteer');
 const creds = require('./constants');
 const low = require('lowdb');
+const numeral = require('numeral');
 const FileSync = require('lowdb/adapters/FileSync');
 
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-const printers = db.get('printers').take(1).value();
+const printers = db.get('printers').value();
 
 async function startBrowser() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true});
   const page = await browser.newPage();
   return {browser, page};
 }
@@ -21,45 +22,64 @@ async function closeBrowser(browser) {
 const ui_type = {}
 
 async function scrapPrinter (printer) {
+  console.log(`Getting ${printer.name} counter`);
   const {browser, page} = await startBrowser();
   page.setViewport({width: 1366, height: 768});
+  await page.setRequestInterception(true);
+
+  page.on('request', (req) => {
+    if(req.resourceType() == 'stylesheet' || req.resourceType() ==  'font' || req.resourceType() == 'image') {
+      req.abort();
+    }
+    else {
+      req.continue();
+    }
+  });
+  try {
+    await page.goto(printer.ip_address);
+  } catch (e) {
+    console.log('Printer not online', e.message);
+    return
+  }
+  console.log(`${printer.name} is online`);
+
   if (!printer.cta) {
     await page.goto(printer.browse_to_address);
     let element = await page.$(printer.counter_field);
-    let text = await page.evaluate(element => element.textContennt, element)
+    let text = await page.evaluate(element => element.innerText, element)
     console.log(printer.name)
-    console.log(text);
+    console.log(numeral(text).format(0,0));
     return
   }
   if (!printer.password) {
-    await page.goto(printer.ip_adress);
+    await page.goto(printer.ip_address);
     await page.click(printer.first_click);
     await page.click(printer.selector_user);
     await page.keyboard.type(printer.user);
     await page.click(printer.cta);
     await page.goto(printer.browse_to_address);
     let element = await page.$(printer.counter_field);
-    let text = await page.evaluate(element => element.textContennt);
+    let text = await page.evaluate(element => element.innerText, element);
     console.log(printer.name);
-    console.log(text);
+    console.log(numeral(text).format(0,0));
     return
   }
-  await page.goto('http://192.168.254.5/');
+  await page.goto(printer.ip_address);
   await page.waitForNavigation();
-  console.log('here');
-  await page.click('#deptid');
+  await page.click(printer.selector_user);
   await page.keyboard.type(printer.user);
-  await page.click('#password');
-  await page.keyboard.type('7654321');
-  await page.click('input.ButtonEnable:nth-child(1)');
-  await page.waitForNavigation();
-  await page.click('.App1_7');
-  await page.waitForNavigation();
+  await page.click(printer.selector_password);
+  await page.keyboard.type(printer.password);
+  await page.click(printer.cta);
+  await page.waitForSelector(printer.next_click);
+  await page.click(printer.next_click);
+  await page.waitForTimeout(2000);
   await page.goto(printer.browse_to_address);
+  await page.waitForSelector(printer.counter_field);
   let element = await page.$(printer.counter_field);
-  let text = await page.evaluate(element => element.textContennt);
+  let text = await page.evaluate(element => element.innerText, element);
   console.log(printer.name);
-  console.log(text);
+  console.log(numeral(text).format(0,0));
   // await page.authenticate({'username': C.username, 'password': C.password})
   // await page.waitForNavigation();
   // await page.screenshot({path: 'calidad_p7.png'});
